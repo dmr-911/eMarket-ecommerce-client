391b593ed5dde7986acedd696dd139b61cae0439
mizan <dewan.mizanur911@gmail.com>
2022-01-13 10:28:14 +0600

    context api implemented

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './Pages/Shared/Navigation/Navigation';
 import Home from './Pages/Home/Home/Home';
+import AuthProvider from './Context/AuthProvider';
 
 function App() {
   return (
     <div className="App">
+      <AuthProvider>
       <Navigation></Navigation>
       <Home></Home>
+      </AuthProvider>
     </div>
   );
 }
