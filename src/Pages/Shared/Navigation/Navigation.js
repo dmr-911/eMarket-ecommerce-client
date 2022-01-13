d70efc6d794b6a010d429f1850c4115b00ef6396
mizan <dewan.mizanur911@gmail.com>
2022-01-13 09:06:08 +0600

    Navigation and footer components created

---
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navigation.css';
+
+const Navigation = () => {
+    return (
+        <div>
+            <h2>This is navigation</h2>
+        </div>
+    );
+};
+
+export default Navigation;
