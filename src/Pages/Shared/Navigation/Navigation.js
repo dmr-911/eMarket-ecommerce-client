a0961d36e7f77d45d836c02bc6f0a94a757ba03f
mizan <dewan.mizanur911@gmail.com>
2022-02-14 01:41:15 +0600

    navbar implemented

--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -7,7 +7,7 @@ import useAuth from '../../../hooks/useAuth';
 const Navigation = () => {
     const {count} = useAuth();
     return (
-        <Navbar className="nav-bg" variant="dark" expand="lg">
+        <Navbar variant="light" expand="lg">
             <Container fluid>
                 <Navbar.Brand href="#">
                 <img
@@ -21,38 +21,24 @@ const Navigation = () => {
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                 <Nav
-                    className="me-auto my-2 my-lg-0"
+                    className="ms-auto my-2 my-lg-0"
                     style={{ maxHeight: '100px' }}
                     navbarScroll
                 >
-                    <Nav.Link href="#action1">Home</Nav.Link>
-                    <Nav.Link href="#action2">Order Review</Nav.Link>
-                    <Nav.Link href="#">
-                    Manage Inventory Here
+                    <Nav.Link href="#login" className="mx-auto">
+                        <span className="nav-icon nav-account">
+                        </span>
                     </Nav.Link>
-                    <Nav.Link href="#login">
-                    Login
+                    <Nav.Link href="#login" className="mx-auto">
+                        <span className="nav-icon nav-wishlist">
+                        </span>
                     </Nav.Link>
-                    <NavDropdown title="name" id="navbarScrollingDropdown">
-                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action5">
-                        Logout
-                    </NavDropdown.Item>
-                    </NavDropdown>
+                    <Nav.Link href="#login" className="mx-auto">
+                        <span className="nav-icon nav-cart">
+                            <span className="nav-text">{count <100 ? count : 'NL'}</span>
+                        </span>
+                    </Nav.Link>
                 </Nav>
-                <Form className="d-flex">
-                    <FormControl
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                    />
-                    <div className="d-flex align-items-center justify-content-center">
-                    <i className="fas fa-shopping-cart fs-5 text-white"></i><Badge bg="secondary">{count}</Badge>
-                    </div>
-                </Form>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
