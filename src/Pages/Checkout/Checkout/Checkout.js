9e5d04057c37c4743f82f25a4e16862d37166fbe
mizan <dewan.mizanur911@gmail.com>
2022-02-15 04:02:02 +0600

    Checkout component createdd

---
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Checkout.css';
+
+const Checkout = () => {
+    return (
+        <div>
+            <h4>Payment option is coming soon!</h4>
+        </div>
+    );
+};
+
+export default Checkout;
